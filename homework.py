c996f4b43a3917b3eb1ff516bdb1793d17abae4d
Anthony Esquivel <aesquivelacademics@gmail.com>
2024-03-21 10:49:25 -0700

    first

---
+++ homework.py
@@ -0,0 +1,40 @@
+#Showcase your dance moves
+weather="sunny"
+if weather=="sunny":
+    print("Wear Sunglasses")
+else:
+     print("Take an Umbrella")
+
+#Python Naming Convention Practice
+pi_value=3.14
+user_age=25
+user_location="New_york"
+MAXLIMIT=1000
+
+#Python Data Types and type() function
+variable_a="Hello World"
+variable_b=23
+variable_c=3.14
+variable_d=True
+print(type(variable_a))
+print(type(variable_b))
+print(type(variable_c))
+print(type(variable_d))
+
+# Arithmetic Operations in Daily Life
+
+#Task1
+Eggs=3.99
+Yogurt=4.50
+one_pound_steak=5.50
+
+#Task2
+savings=10000
+interest_rate=.07
+total=(savings*interest_rate+savings)
+print(total)
+
+#Understand Assignments an Comparison
+a=56
+b=56
+print(a==b)
